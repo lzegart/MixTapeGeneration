15491ff66b08a3282656639bb3d5d34fafae945b
Lucah Endicott <lucahdavid88@gmail.com>
2021-01-21 22:13:45 -0800

    add create tables in each api route file

--- routes/songs-api-routes.js
+++ routes/songs-api-routes.js
@@ -1,11 +1,16 @@
 const db = require('../models');
 
 module.exports = (app) => {
+    //not sure if we need this to be /api/songs or technically /api/playlist???
+    app.post('/api/songs', (req, res) => {
+        db.Songs.create(req.body).then((dbsongs) => res.json(dbsongs));
+    });
+
     app.delete('/api/songs/:id', (req, res) => {
         db.Songs.destroy({
             where: {
             id: req.params.id,
             },
-        }).then((dbPlaylist) => res.json(dbPlaylist));
+        }).then((dbsongs) => res.json(dbsongs));
     });
 }
