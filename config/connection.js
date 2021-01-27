92adf527e1fc24f9335a0acf4a4beda48fdd5f17
Francisco Rones <ininorones@gmail.com>
2021-01-26 20:39:37 -0800

    Update: getplaylist and save song added

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "P123456p123456!",
-  password: "password",
+  user: "root",
+  password: "P123456p123456!",
   database: "MTG_db",
 });
 
--- models/playlist.js
+++ models/playlist.js
@@ -1,37 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
-    const Playlist = sequelize.define('Playlist', {
-        id: {
-        type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true,
-        },
-        playlist_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 20]
-            } 
-        }, 
-        song_list: {
-            type: Array,
-            default: [],
-        }
+  const Playlist = sequelize.define("Playlist", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    playlist_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 20],
+      },
+    },
+    // song_list: {
+    //     type: Array,
+    //     default: [],
+    // }
 
-        //
-        // user_id: {
-        //     type: DataTypes.INTEGER,
-        //     // allowNull: false,
-        // },
+    //
+    // user_id: {
+    //     type: DataTypes.INTEGER,
+    //     // allowNull: false,
+    // },
+  });
+
+  Playlist.associate = (models) => {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Playlist.hasMany(models.Songs, {
+      onDelete: "cascade",
     });
+  };
 
-    Playlist.associate = (models) => {
-        // Associating Author with Posts
-        // When an Author is deleted, also delete any associated Posts
-        Playlist.hasMany(models.Songs, {
-            onDelete: 'cascade',
-        });
-    };
-
-    return Playlist;
+  return Playlist;
 };
